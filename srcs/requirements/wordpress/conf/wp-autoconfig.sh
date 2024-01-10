6c1b7e3ff9a3f8b3d199e49bcaa6ae4838757393
drogonj <drogonj@LAPTOP-MDM7N8FS>
2024-01-10 22:42:28 +0100

    volumes.....................

--- srcs/requirements/wordpress/conf/wp-autoconfig.sh
+++ srcs/requirements/wordpress/conf/wp-autoconfig.sh
@@ -4,8 +4,9 @@ echo "running wp-autoconfig.sh script..."
 sleep 10
 
 wp config create --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_USER_PASSWORD} --dbhost=mariadb --extra-php <<PHP
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 PHP
 wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=admin --admin_password=${SQL_ROOT_PASSWORD} --admin_email=adm@ngalzand.42.fr
 wp user create --allow-root ${SQL_USER} ${SQL_USER}@${HOSTNAME} --role=author --user_pass=${SQL_USER_PASSWORD}
