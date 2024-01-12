bf5e71bd73173cc90bde5de98b4311bb9db6504a
drogonj <drogonjm@gmail.com>
2024-01-12 16:53:07 +0100

    wawawawawa

--- srcs/requirements/mariadb/conf/mariadb_init.sh
+++ srcs/requirements/mariadb/conf/mariadb_init.sh
@@ -3,36 +3,33 @@
 echo "Loading mariadb initialization script..."
 /etc/init.d/mariadb start
 
-until mysqladmin ping &> /dev/null; do
-        sleep 0.5
-done
+if [ -f ./db_configured ]; then
+	echo "mariadb is already configured."
+else	
+	until mysqladmin ping &> /dev/null; do
+	        sleep 0.5
+	done
+	echo "Setting root password"
+	mysql -uroot -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${SQL_ROOT_PASSWORD}');"
+	sleep 0.5	
+	mysql -uroot -e "FLUSH PRIVILEGES;"
+	sleep 0.5
+	echo "Searching/Creating database '${SQL_DATABASE}'"
+	mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+	sleep 0.5
+	echo "Searching/Creating user '${SQL_USER}'"
+	mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost';"
+	sleep 0.5
+	mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "SET PASSWORD FOR '${SQL_USER}'@'localhost' = PASSWORD('${SQL_USER_PASSWORD}');"
+	sleep 0.5
+	echo "Setting '${SQL_USER}' privileges"
+	mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'wordpress.srcs_inception' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
+	sleep 0.5	
+	mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+	sleep 0.5
+	touch db_configured
+fi
 
-echo "Setting root password"
-mysql -uroot -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${SQL_ROOT_PASSWORD}');"
-sleep 0.5
-
-mysql -uroot -e "FLUSH PRIVILEGES;"
-sleep 0.5
-
-echo "Searching/Creating database '${SQL_DATABASE}'"
-mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-sleep 0.5
-
-echo "Searching/Creating user '${SQL_USER}'"
-mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost';"
-sleep 0.5
-mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "SET PASSWORD FOR '${SQL_USER}'@'localhost' = PASSWORD('${SQL_USER_PASSWORD}');"
-sleep 0.5
-
-echo "Setting '${SQL_USER}' privileges"
-mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'wordpress.srcs_inception' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-sleep 0.5
-
-mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-sleep 0.5
-
-echo "Restarting mysql in safe mode"
-mysqladmin -uroot -p"${SQL_ROOT_PASSWORD}" shutdown
-sleep 0.5
+mysqladmin -uroot -p"{SQL_ROOT_PASSWORD}" shutdown
 
 exec "$@"
--- srcs/requirements/wordpress/conf/wp-autoconfig.sh
+++ srcs/requirements/wordpress/conf/wp-autoconfig.sh
@@ -3,14 +3,17 @@
 echo "running wp-autoconfig.sh script..."
 sleep 10
 
-wp config create --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_USER_PASSWORD} --dbhost=mariadb --extra-php <<PHP
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
-PHP
-wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=admin --admin_password=${SQL_ROOT_PASSWORD} --admin_email=adm@ngalzand.42.fr
+if [ -f /var/www/html/wp-config.php ]; then
+	echo "wordpress is already configured."
+	echo "done."
+	/usr/sbin/php-fpm7.4 -F
+fi
+
+wp config create --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_USER_PASSWORD} --dbhost=mariadb
+wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=o5 --admin_password=${SQL_ROOT_PASSWORD} --admin_email=adm@ngalzand.42.fr
 wp user create --allow-root ${SQL_USER} ${SQL_USER}@${HOSTNAME} --role=author --user_pass=${SQL_USER_PASSWORD}
 wp plugin update --allow-root --all
 
 echo "done."
-exec "$@"
+
+/usr/sbin/php-fpm7.4 -F
